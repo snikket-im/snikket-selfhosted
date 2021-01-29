ac4ee13f230b041dbacfccd73a65941dff8b7a13
Matthew Wild <mwild1@gmail.com>
2021-01-29 12:39:05 +0000

    Detect docker/docker-compose

--- init.sh
+++ init.sh
@@ -8,6 +8,35 @@ if test -f docker-compose.yml; then
 	exit 1;
 fi
 
+## Platform detection ##
+OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+if [ -z "$OS" ]; then
+    OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')
+fi
+
+if [ -z "$VERSION" ]; then
+    VERSION=$(awk '{print $3}' /etc/*-release)
+fi
+########################
+
+
+if ! which docker; then
+	echo "Docker is required but not installed."
+	case "$OS" in
+	ubuntu|debian|fedora|centos)
+		echo "Please follow the guide at https://docs.docker.com/install/linux/docker-ce/$OS/" ;;
+	*)
+		echo "Please follow the installation guide at https://docs.docker.com/engine/install/" ;;
+	esac
+	exit 1;
+fi
+
+if ! which docker-compose; then
+	echo "Docker Compose (docker-compose) is required but not installed."
+	echo "Please follow the installation guide at https://docs.docker.com/compose/install/"
+	exit 1;
+fi
+
 mv docker-compose.base.yml docker-compose.yml
 
 exit 0;
