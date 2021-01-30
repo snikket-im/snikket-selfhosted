94d4c38af5e07bf97516fde47950e3a891daf56a
Matthew Wild <mwild1@gmail.com>
2021-01-30 13:09:49 +0000

    new-invite.sh: Add a script to generate a new invite link

---
+++ scripts/new-invite.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SNIKKET_DOMAIN=$(grep ^SNIKKET_DOMAIN= snikket.conf | cut -d= -f2)
+
+if [[ -z "$SNIKKET_DOMAIN" ]]; then
+	echo "Failed to read SNIKKET_DOMAIN from snikket.conf, unable to continue";
+	exit 1;
+fi
+
+exec docker exec -it snikket prosodyctl mod_invites generate "$SNIKKET_DOMAIN" $*
